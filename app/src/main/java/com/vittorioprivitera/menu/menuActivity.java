54de82f48e4072b0f88d18be92ae8df4fff06858
vittori02-ui <vittorioprivitera02@gmail.com>
2026-08-12 13:19:08 +0200

    first part of grafic( first page ) and put the second

---
+++ app/src/main/java/com/vittorioprivitera/menu/menuActivity.java
@@ -0,0 +1,14 @@
+package com.vittorioprivitera.menu;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class menuActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.menu_activity);
+    }
+}
