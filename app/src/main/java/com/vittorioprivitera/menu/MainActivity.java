71c81f16275b366f6eafe933d7bf6d2e5296c942
vittori02-ui <vittorioprivitera02@gmail.com>
2026-08-13 13:51:22 +0200

    end of the first screen and i start the code

--- app/src/main/java/com/vittorioprivitera/menu/MainActivity.java
+++ app/src/main/java/com/vittorioprivitera/menu/MainActivity.java
@@ -2,13 +2,36 @@ package com.vittorioprivitera.menu;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Spinner;
 
 public class MainActivity extends AppCompatActivity {
 
+    Spinner elenTavoli,elenSala;
+    EditText clienti;
+    Button avanti;
+    Intent act;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        elenTavoli=findViewById(R.id.elencoTavoli);
+        elenSala=findViewById(R.id.elencoSale);
+        clienti=findViewById(R.id.numClienti_txt);
+        avanti=findViewById(R.id.buttonAvanti);
+        avanti.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                act=new Intent(MainActivity.this,menuActivity.class);
+                act.putExtra("sala",elenSala.getSelectedItem().toString());
+                act.putExtra("tavolo",elenTavoli.getSelectedItem().toString());
+                act.putExtra("clienti",clienti.getText().toString());
+                startActivity(act);
+            }
+        });
     }
 }
--- app/src/main/java/com/vittorioprivitera/menu/menuActivity.java
+++ app/src/main/java/com/vittorioprivitera/menu/menuActivity.java
@@ -3,12 +3,14 @@ package com.vittorioprivitera.menu;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class menuActivity extends AppCompatActivity {
-
+    TextView lb1,lb2,lb3;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.menu_activity);
+
     }
 }
