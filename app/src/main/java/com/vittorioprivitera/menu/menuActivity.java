ac2fc9b63829b1895f9b19cac5e0507dea4539ba
vittori02-ui <vittorioprivitera02@gmail.com>
2026-08-14 13:12:55 +0200

    start with the menu and I completed the first screen

--- app/src/main/java/com/vittorioprivitera/menu/menuActivity.java
+++ app/src/main/java/com/vittorioprivitera/menu/menuActivity.java
@@ -1,14 +1,18 @@
 package com.vittorioprivitera.menu;
-
 import static java.lang.Integer.parseInt;
-
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.cardview.widget.CardView;
 
 import android.os.Bundle;
+import android.view.View;
 import android.widget.TextView;
-
+import android.widget.Button;
+import android.content.Intent;
 public class menuActivity extends AppCompatActivity {
     TextView lb1,lb2,lb3;
+    Button bIndietro;
+    Intent act;
+    CardView menu;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -16,11 +20,19 @@ public class menuActivity extends AppCompatActivity {
         lb1=findViewById(R.id.lb1);
         lb2=findViewById(R.id.lb2);
         lb3=findViewById(R.id.lb3);
+        bIndietro=findViewById(R.id.bIndietro);
         String sala=getIntent().getStringExtra("sala");
         String tav=getIntent().getStringExtra("tavolo");
         int cli=parseInt(getIntent().getStringExtra("clienti"));
-        lb1.setText("La sala è "+sala);
-        lb2.setText("Il tavolo è "+tav);
-        lb3.setText("I clienti sono "+cli);
+        lb1.setText(sala);
+        lb2.setText(tav);
+        lb3.setText("Clienti: "+cli);
+        bIndietro.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                act=new Intent(menuActivity.this,MainActivity.class);
+                startActivity(act);
+            }
+        });
     }
 }
